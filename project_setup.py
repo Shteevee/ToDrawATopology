7096ac91d78d06de472dc7bb89e391857daf44d7
Shteevee <shteve451@gmail.com>
2019-03-04 04:33:23 +0000

    take 10

--- project_setup.py
+++ project_setup.py
@@ -3,13 +3,16 @@ import sys
 
 def main():
     required_packages = ["numpy", "scikit-image", "matplotlib", "svgwrite"]
-    pip_path = '../../../python/scripts/pip'
+    if len(sys.argv) > 2:
+        pip_path = sys.argv[1]
+    else:
+        pip_path = '../../../python/scripts/pip'
 
-    if sys.argv[1] == 'install':
+    if sys.argv[-1] == 'install':
         for required_package in required_packages:
             subprocess.call([pip_path, 'install', required_package])
 
-    elif sys.argv[1] == 'uninstall':
+    elif sys.argv[-1] == 'uninstall':
         for required_package in required_packages:
             subprocess.call([pip_path, 'uninstall', required_package])
 
